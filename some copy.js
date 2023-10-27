6865317854e15d0167068ba85cd2718351da9c38
Vtheonly <merselfaresw@gmail.com>
2023-10-27 19:45:42 +0100

    l

---
+++ some copy.js
@@ -0,0 +1,17 @@
+console.log("==============");
+console.log("-- VERSION 1 same shit difffent day--");
+console.log("==============");
+// bflwbflw
+// efjwefbwelf
+// efwevfwelf
+// fljwebfwelofbw3loeff
+
+
+
+// qeqbfbebfiubef  obouf
+// wefblwebflwebfl
+
+
+
+
+
